c70b55f00cef9ffac6668465829f86ac924ea5a8
unknown <chotjd329@hotmail.com>
2021-01-03 22:08:13 +0900

    naver news search and using db

--- stock/news/models.py
+++ stock/news/models.py
@@ -2,9 +2,12 @@ from django.db import models
 
 
 class News(models.Model):
-    subject = models.CharField(max_length=200)
+    subject = models.TextField()
     content = models.TextField()
-    create_date = models.DateTimeField()
+    created_date = models.TextField()
+    keyword = models.TextField()
+    url = models.TextField()
+
 
     def __str__(self):
         return self.subject
