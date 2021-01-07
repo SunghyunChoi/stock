2d31e6d442a7a866cd34cd6a549c5616590be99f
karockai <75655052+karockai@users.noreply.github.com>
2021-01-07 20:52:23 +0900

    1/7 페이징, 댓글갯수 보이기

--- stock/mainboard/views.py
+++ stock/mainboard/views.py
@@ -3,12 +3,25 @@ from django.http import HttpResponse
 from django.utils import timezone
 from .forms import ArticleForm, CommentForm
 from .models import Article, Comment
+from django.core.paginator import Paginator
+
+
 
 
 
 def index(request):
     article_list = Article.objects.order_by('-create_date')
-    context = {'article_list' : article_list}
+
+        # 입력 파라미터
+    page = request.GET.get('page', '1')  # 페이지
+
+        # 페이징처리
+    paginator = Paginator(article_list, 10)  # 페이지당 10개씩 보여주기
+    page_obj = paginator.get_page(page)
+
+    context = {'article_list': page_obj}
+    # context = {'article_list' : article_list} <- 페이징하면서 사라짐
+
 
     # return HttpResponse("안녕하세요 pybo에 오신것을 환영합니다.")
     return render(request, 'article_list.html', context)
