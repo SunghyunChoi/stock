ae66efe4c76d456f1242b937f76ea4afa3f26520
sunghyunchoi <chltjdgus99@naver.com>
2021-01-09 00:12:15 +0900

    1/8 부트스트랩 및 회원가입 기능 추가

--- stock/common/views.py
+++ stock/common/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.shortcuts import render, redirect
+from common.forms import UserForm
 
-# Create your views here.
+
+def signup(request):
+    """
+    계정생성
+    """
+    if request.method == "POST":
+        form = UserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username') # 이름 가져오기
+            raw_password = form.cleaned_data.get('password1') # 비밀번호 가져오기
+            user = authenticate(username=username, password=raw_password) # 회원가입 후 자동 로그인 : authenticate
+            login(request, user)
+            return redirect('mainboard:index')
+    else:
+        form = UserForm()
+    return render(request, 'common/signup.html', {'form': form})
