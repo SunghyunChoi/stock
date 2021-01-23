dcf221ec35e9a343f1ae1c89c4032ebf51d9d56f
tongky <chotjd329@hotmail.com>
2021-01-23 10:55:57 +0900

    slack bot jsonify

--- stock/modules/slack_bot.py
+++ stock/modules/slack_bot.py
@@ -27,55 +27,37 @@ from pandas.io.json import json_normalize
 # # """)
 class slack_bot:
         def __init__(self):
-                self.token = "xoxb-1543956691168-1628388530791-qmoRZSziF3fD0csLF4O2XjYV"
+                self.token = "xoxb-1543956691168-1628388530791-lJRTT2tj5oSpJ3ouHGbB31Cv"
                 self.channel_id = "G01HZ0FBL9J"
         def send(self, message):
-                self.data = {'Content-Type': 'application/x-www-form-urlencoded',
-                        'token': self.token,
-                        'channel': self.channel_id, 
-                        'message': message
+                self.headers = {
+                        "Content-type": "application/json; charset=utf-8",
+                        "Authorization": f"Bearer {self.token}"
                 }
-
+                self.data = {
+                        "token": self.token,
+                        "channel": self.channel_id,
+                        "text": "test",
+                        "attachments": [
+                                {
+                                "fallback": "680x448px image",
+                                "image_url": "https://newsimg.hankookilbo.com/cms/articlerelease/2019/04/29/201904291390027161_3.jpg",
+                                "text": "text-world",
+                                "pretext": "pre-hello"
+                                }
+                        ]
+                }
                 URL = "https://slack.com/api/chat.postMessage"
-                res = requests.post(URL, data=self.data)
+                res = requests.post(URL, headers=self.headers,data=json.dumps(self.data))
+                # res = requests.post(URL, data=self.data)
+
                 print(res)
+                print(res.json())
+
 
 # {datetime.today().strftime("%m/%d %H:%M")}
-message ="""
-{
-        "bot_id": "B01JXATM7AP",
-        "type": "message",
-        "text": "test",
-        "user": "U01JGBEFLP9",
-        "ts": "1611189923.000500",
-        "team": "T01FZU4LB4Y",
-        "bot_profile": {
-            "id": "B01JXATM7AP",
-            "deleted": false,
-            "name": "eco",
-            "updated": 1610759123,
-            "app_id": "A01KLV7KSJC",
-            "icons": {
-                "image_36": "https://a.slack-edge.com/80588/img/plugins/app/bot_36.png",
-                "image_48": "https://a.slack-edge.com/80588/img/plugins/app/bot_48.png",
-                "image_72": "https://a.slack-edge.com/80588/img/plugins/app/service_72.png"
-            },
-            "team_id": "T01FZU4LB4Y"
-        },
-        "attachments": [
-            {
-                "fallback": "680x448px image",
-                "image_url": "https://newsimg.hankookilbo.com/cms/articlerelease/2019/04/29/201904291390027161_3.jpg",
-                "image_width": 680,
-                "image_height": 448,
-                "image_bytes": 170105,
-                "text": "text-world",
-                "pretext": "pre-hello",
-                "id": 1
-            }
-        ]
-    }
+# message ="""
 
-"""
+message = "타이푼은 태풍입니다"
 test = slack_bot()
 test.send(message)
