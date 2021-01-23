0bd354842971f74ccc092e471ee5fa781baaa28d
syeeuns <syeeuns@gmail.com>
2021-01-23 11:31:11 +0900

    graph logic, png add logic

--- stock/exchange/models.py
+++ stock/exchange/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
 
+
 class Stock_list(models.Model):
     market_name = models.CharField(max_length=50)
     symbol = models.IntegerField(max_length=50)
