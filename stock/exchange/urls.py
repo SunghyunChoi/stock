98e7fc88990e95aec92862c16f636b044319dbbb
sunghyunchoi <chltjdgus99@naver.com>
2021-01-25 09:48:14 +0900

    urls

--- stock/exchange/urls.py
+++ stock/exchange/urls.py
@@ -2,11 +2,11 @@ from django.urls import path
 
 from . import views
 
-app_name = 'mainboard'
+app_name = 'exchange'
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('<int:article_id>/', views.detail, name='detail'),
-    path('article/create/', views.article_create, name='article_create'),
-    path('comment/create/<int:article_id>/', views.comment_create, name='comment_create'),
+    #path('<int:article_id>/', views.detail, name='detail'),
+    #path('article/create/', views.article_create, name='article_create'),
+    #path('comment/create/<int:article_id>/', views.comment_create, name='comment_create'),
 ]
