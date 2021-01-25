e6d49983ccffb42dcec59322a1064fc487a89c26
karockai <75655052+karockai@users.noreply.github.com>
2021-01-25 10:28:30 +0900

    1/25 종료

--- stock/exchange/views.py
+++ stock/exchange/views.py
@@ -1,10 +1,3 @@
-from django.shortcuts import render
-
-# Create your views here.
-def index(request):
-    
-    # return HttpResponse("안녕하세요 pybo에 오신것을 환영합니다.")
-    return render(request, 'stocklist.html')
 ##### 주식 데이터, 그래프 관련 #####
 import io
 import urllib, base64
@@ -14,9 +7,20 @@ import matplotlib.gridspec as gridspec
 import matplotlib.ticker as ticker
 from mplfinance.original_flavor import candlestick2_ochl
 ########################################################
+from django.shortcuts import render
+from django.http import HttpResponse
 
+# Create your views here.
+def index(request):
+    
+    # return HttpResponse("안녕하세요 pybo에 오신것을 환영합니다.")
+    return render(request, 'exchange/stocklist.html')
+    # return HttpResponse("exchange")
 # 그래프 보여주는 부분 (수정 예정)
 # home.html(가칭)에 img src 에도 추가해줘야함
+
+
+
 def home(request):
     plt.plot(range(10))
     fig = plt.gcf()
