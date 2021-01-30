56db41c0f7adcb0b810489dcb512ef1c03e66e14
syeeuns <syeeuns@gmail.com>
2021-01-30 14:53:28 +0900

    cur_price update 21.01.29

--- stock/cur_price_update.py
+++ stock/cur_price_update.py
@@ -16,16 +16,25 @@ from datetime import datetime
 
 df_kospi = fdr.StockListing('KOSPI')
 df_simple = df_kospi.iloc[:, [0, 1, 2]]  # 종목번호, 마켓, 종목이름
+
+# 실행시킬때마다 오늘 종가기준 가격 업데이트
+# 토,일에 실행할 경우, empty dataframe 때문에 오류 발생하므로 cron 설정 시 월~금만 실행하도록 설정할 것
+# cron 설정은 서버에서 해야하므로 통키와 상의해볼 것
 today = datetime.today().strftime('%Y-%m-%d')
 stock_list = Stock_list.objects.all()
-for one in stock_list:
-    stock_id = str(one.symbol)
-    df_specific = fdr.DataReader(stock_id, today)
+for i in range(len(stock_list)):
+    stock_id = str(stock_list[i].symbol)
     try:
-        print(df_specific)
-        one.cur_price = int(df_specific['Close'][0])
-        one.save()
-    except:
-        print("ERROR")
-        print(df_specific)
-    
+        df_specific = fdr.DataReader(stock_id, today)
+        stock_list[i].cur_price = int(df_specific['Close'][0])
+        stock_list[i].save()
+        print(stock_list[i].stock_name, stock_list[i].cur_price, "good\n")
+    except ValueError:
+        print("ValueError", stock_list[i].stock_name)
+        # print(df_specific)
+        pass
+    except KeyError:
+        print("KeyError", stock_list[i].stock_name)
+        # print(df_specific)
+        pass
+
