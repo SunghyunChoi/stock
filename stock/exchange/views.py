25e22aaaf6d1b5e6dee5904e34f130c700a5ed8c
syeeuns <syeeuns@gmail.com>
2021-02-01 10:45:03 +0900

    add candle chart

--- stock/exchange/views.py
+++ stock/exchange/views.py
@@ -2,6 +2,8 @@
 import io
 import urllib, base64
 import FinanceDataReader as fdr
+import matplotlib
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
 import matplotlib.ticker as ticker
@@ -15,6 +17,7 @@ from common.models import MyUser
 from django.utils import timezone
 from django.core.paginator import Paginator # 페이징 관련 (글 목록)
 from django.db.models import Q # 검색 관련
+import datetime
 
 
 # Create your views here.
@@ -54,13 +57,11 @@ def index(request):
 # ----------------------------------- [페이징] --------------------------- #
 
 
-# return HttpResponse("exchange")
-# 그래프 보여주는 부분 (수정 예정)
-# home.html(가칭)에 img src 에도 추가해줘야함
-
+# 예은 : 그래프 만드는 부분 추가했습니다
 def stock_list_page(request, symbol):
     stock = Stock_list.objects.get(symbol=symbol)
-    context = {'stock': stock}
+    graph_uri = graph(request, symbol)
+    context = {'stock': stock, 'data': graph_uri}
     return render(request, 'exchange/stock_page.html', context)
 
 
@@ -74,7 +75,44 @@ def find_stock(request):
     return render(request, 'exchange/stock_list_page.html', context)
 
 
-def home(request):
+def graph(request, symbol):
+    stock = Stock_list.objects.get(symbol=symbol)
+
+    # 봉차트 그리기
+    # 음봉이 안나와요 선생님 ..  수정 .. 해야합니다 ..
+    now = datetime.datetime.now()
+    half_year = (now - datetime.timedelta(days=180)).strftime('%Y-%m-%d')
+    df_specific = fdr.DataReader(symbol, half_year)
+
+    df_specific['MA3'] = df_specific['Close'].rolling(3).mean()
+    df_specific['MA5'] = df_specific['Close'].rolling(5).mean()
+    df_specific['MA10'] = df_specific['Close'].rolling(10).mean()
+    df_specific['MA60'] = df_specific['Close'].rolling(60).mean()
+    df_specific[['Close','MA3','MA5','MA10']].plot()
+
+    fig = plt.figure(figsize=(20,10))
+    ax = fig.add_subplot(111)
+    index = df_specific.index.astype('str') # 캔들스틱 x축이 str로 들어감
+
+    # 이동평균선 그리기
+    ax.plot(index, df_specific['MA3'], label='MA3', linewidth=0.7)
+    ax.plot(index, df_specific['MA5'], label='MA5', linewidth=0.7)
+    ax.plot(index, df_specific['MA10'], label='MA10', linewidth=0.7)
+
+    # X축 티커 숫자 20개로 제한
+    ax.xaxis.set_major_locator(ticker.MaxNLocator(20))
+
+    # 그래프 title과 축 이름 지정
+    # ax.set_title(stock_name, fontsize=22)
+    ax.set_xlabel('Date')
+
+    # 캔들차트 그리기
+    candlestick2_ochl(ax, df_specific['Open'], df_specific['High'],
+                      df_specific['Low'], df_specific['Close'],
+                      width=0.5, colorup='r', colordown='b')
+    ax.legend()
+    plt.grid()
+
     plt.plot(range(10))
     fig = plt.gcf()
     buf = io.BytesIO()
@@ -82,7 +120,7 @@ def home(request):
     buf.seek(0)
     string = base64.b64encode(buf.read())
     uri = urllib.parse.quote(string)
-    return render(request, 'home.html', {'data':uri})
+    return uri
 
 
 # get username, stockID, quantity from get request 
